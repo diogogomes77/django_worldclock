6c0256048e1df6727521f94321c42325e48dad16
Diogo Gomes <diogo.gomes77@gmail.com>
2020-05-30 18:01:00 +0100

    models and admin showing time of selected country timezones

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import ChosenCountry, Profile, Country
+
+
+class ChosenCountryInline(admin.TabularInline):
+    model = ChosenCountry
+    extra = 1
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [ChosenCountryInline, ]
+
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class ProfileInline(admin.TabularInline):
+    model = Country.profiles.through
+    extra = 1
+
+
+class CountryAdmin(admin.ModelAdmin):
+    inlines = [ProfileInline, ]
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+#class ChosenCountryAdmin(admin.ModelAdmin):
+
+admin.site.register(ChosenCountry)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,90 @@
+from django.contrib.auth.models import User
 from django.db import models
+from timezone_field import TimeZoneField
+import datetime
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    timezone = TimeZoneField(default='Europe/Lisbon')
+
+    class Meta:
+        verbose_name = "country"
+        verbose_name_plural = "countries"
+
+    def __str__(self):
+        return self.name
+
+
+class ChosenCountry(models.Model):
+    FORMATS = (
+        (1, "%H:%M:%S"),
+        (2, "%l:%M:%p"),
+        (3, "%r"),
+        (4, "%R"),
+        (5, "%T")
+    )
+    # better put this on forms to prevent large generated migrations
+    #choices = [
+    #    (timezone, timezone)
+    #    for timezone in pytz.common_timezones
+    #],
+    country = models.ForeignKey(
+        'Country',
+        on_delete=models.CASCADE
+    )
+    profile = models.ForeignKey(
+        'Profile',
+        on_delete=models.CASCADE
+    )
+    format = models.IntegerField(choices=FORMATS, default=1)
+
+    def __str__(self):
+        timezone = self.country.timezone
+        now = datetime.datetime.now(timezone)
+        now = now.strftime(self.get_format_display())
+        return now
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    countries = models.ManyToManyField(
+        'Country',
+        related_name='profiles',
+        through=ChosenCountry,
+        through_fields=["profile", "country"]
+    )
+
+    def __str__(self):
+        return self.user.username
+
+    def add_country(self, country):
+        total_countries = self.countries.count()
+        # check if 5 to 10 different countries
+        if total_countries < 5 or total_countries > 10:
+            return False
+
+        # creates hascountry preventing duplicates
+        ChosenCountry.objects.get_or_create(
+            country=country,
+            profile=self
+        )
+        return True
+
+    def del_country(self, country):
+        total_countries = self.countries.count()
+        # prevents having less than 5 countries
+        if total_countries < 6:
+            return False
+        qs_delete = ChosenCountry.objects.filter(
+            country=country,
+            profile=self
+        )
+        if qs_delete.count() == 0:
+            return False
+        qs_delete.delete()
+        return True
+
